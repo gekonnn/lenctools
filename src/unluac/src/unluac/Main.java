ea5357d6ff0676083512a88f63a05ddfb0ddbae4
gekonnn <gekonn13@gmail.com>
2026-01-06 02:42:16 +0100

    unluac: rename version

--- src/unluac/src/unluac/Main.java
+++ src/unluac/src/unluac/Main.java
@@ -24,7 +24,7 @@ import unluac.util.FileUtils;
 
 public class Main {
 
-  public static String version = "1.2.3.530 unluac";
+  public static String version = "1.2.3.530 lenctools";
   
   public static void main(String[] args) {
     String fn = null;
